d77be86b2144b04c423db03d5316acb22ee94c97
Hadrien Salem <48864485+SnowHawkeye@users.noreply.github.com>
2024-09-10 15:33:16 +0200

    [DATA] #39 Type 2 diabetes data extraction

--- src/features/mimic/scripts/samples_creation/create_sample_patient_lab_records.py
+++ src/features/mimic/scripts/samples_creation/create_sample_patient_lab_records.py
@@ -1,11 +1,18 @@
+import pandas as pd
+
 from datasets.mimic_dataset import MimicDataset
 from features.mimic.extract_lab_records import extract_lab_records, save_pickle
 
 
+def get_sample_analyses():
+    return pd.read_parquet("../data/sample/analyses_sample.parquet")
+
+
 def main():
-    dataset = MimicDataset.from_config_file("config/mimic_dataset.mipha.json")
+    dataset = MimicDataset.from_config_file("../config/mimic_dataset.mipha.json")
 
     print("Loading analyses table...")
+    dataset.get_analyses = get_sample_analyses
     analyses = dataset.get_analyses()
     print("Analyses table loaded!")
 
@@ -15,7 +22,7 @@ def main():
         aggfunc='mean'
     )
 
-    save_pickle(patient_dataframes, "data/patient_lab_records.pkl")
+    save_pickle(patient_dataframes, "../data/sample/sample_patient_lab_records.pkl")
 
 
 if __name__ == '__main__':
